5af63418f13896006dc2046d6484400067906f47
Emilien52 <emiliengarandeau@yahoo.fr>
2022-10-02 14:10:06 +0200

    Mise en place nouveau système de traduction sur en.php et fr.php
    La traduction en anglais sera faite prochainement...

---
+++ lang/en.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+	"dashboard" => [
+		"title" => "Actualités",
+		"title_add" => "Actualités - Ajouter une actualités",
+		"title_edit" => "Actualités - Modifier une actualités",
+		"desc" => "Gérez les actualités de votre site",
+	],
+	"list" => [
+		"list" => "Liste des actualités",
+	],
+	"table" => [
+		"title" => "Titre",
+		"description" => "Description",
+		"author" => "Auteur",
+		"creation_date" => "Date de création",
+		"edit" => "Modifications",
+	],
+	"add" => [
+		"title" => "Titre de l'actualité",
+		"title_placeholder" => "Titre",
+		"desc" => "Courte description de l'actualité",
+		"desc_placeholder" => "Description",
+		"enable_comm" => "Activer les commentaires",
+		"enable_likes" => "Activer les likes",
+		"image" => "Choisissez l'image de couverture",
+		"select_image" => "Choisissez une image",
+		"content" => "Contenu",
+	],
+];
