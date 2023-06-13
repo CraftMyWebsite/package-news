12092af39bc3f23a8e72a06a00d6ac2c6616365b
Teyir <60007549+Teyir@users.noreply.github.com>
2023-06-14 00:32:34 +0200

    [OPTIMIZATION] A lot of improves

--- Example/list.view.php
+++ Example/list.view.php
@@ -1,10 +1,13 @@
 <!------------------------------------
     ----- Required namespace-----
 -------------------------------------->
-<?php 
-    use CMW\Controller\Users\UsersController;
-    use CMW\Manager\Security\SecurityManager;
-    $newsList = $newsModel->getSomeNews("NUMBER_OF_NEWS_YOU_WANT_TO_SHOW_MAX", 'DESC');
+<?php
+
+use CMW\Controller\Users\UsersController;
+use CMW\Manager\Env\EnvManager;
+
+/* @var  $newsModel */
+$newsList = $newsModel->getSomeNews("NUMBER_OF_NEWS_YOU_WANT_TO_SHOW_MAX", 'DESC');
 ?>
 
 
@@ -15,17 +18,20 @@
     <img src="<?= $news->getImageLink() ?>" alt="..."/>
     <?= $news->getAuthor()->getPseudo() ?>
     <?= $news->getDateCreated() ?>
-    <a href="<?= Utils::getEnv()->getValue("PATH_SUBFOLDER") ?>news/<?= $news->getSlug() ?>"></a>
+    <a href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>news/<?= $news->getSlug() ?>"></a>
     <?= $news->getTitle() ?>
     <?= $news->getDescription() ?>
-    <a href="<?= Utils::getEnv()->getValue("PATH_SUBFOLDER") ?>news/<?= $news->getSlug() ?>"></a>
-    <?= $news->getLikes()->getTotal() ?>    
+    <a href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>news/<?= $news->getSlug() ?>"></a>
+    <?= $news->getLikes()->getTotal() ?>
 
-    <!--YOU CAN CHECK LIKE THIS FOR LIKES -->                        
+    <!--YOU CAN CHECK LIKE THIS FOR LIKES -->
     <?php if ($news->getLikes()->userCanLike()): ?>
-        <?php if(UsersController::isUserLogged()) {echo "You already love!";} else {echo "Log in to like!";} ?>
-        <?php else: ?> 
-            <a href="<?= $news->getLikes()->getSendLike() ?>">You will like</a>
-        <?php endif; ?>
+        <?php if (UsersController::isUserLogged()) {
+            echo "You already love!";
+        } else {
+            echo "Log in to like!";
+        } ?>
+    <?php else: ?>
+        <a href="<?= $news->getLikes()->getSendLike() ?>">You will like</a>
     <?php endif; ?>
 <?php endforeach; ?>
--- Example/using_in_other_page.php
+++ Example/using_in_other_page.php
@@ -1,17 +1,21 @@
 <!------------------------------------
     ----- Required namespace-----
 -------------------------------------->
-<?php 
+<?php
+
+use CMW\Controller\Core\PackageController;
 use CMW\Controller\Users\UsersController;
+use CMW\Manager\Env\EnvManager;
+use CMW\Model\Core\ThemeModel;
+use CMW\Model\News\NewsModel;
 
 /*!!! COMPATIBILITY : You have to check if the package is installed before use !!!*/
 /*Check installed package*/
-use CMW\Controller\Core\PackageController;
 /*NEWS BASIC NEED*/
-use CMW\Model\News\NewsModel;
+
 if (PackageController::isInstalled("news")) {
     $newsList = new newsModel;
-    $newsList = $newsList->getSomeNews( ThemeModel::fetchConfigValue('news_number_display'));
+    $newsList = $newsList->getSomeNews(ThemeModel::fetchConfigValue('news_number_display'));
 }
 ?>
 
@@ -24,14 +28,17 @@ if (PackageController::isInstalled("news")) {
         <img src="<?= $news->getImageLink() ?>" alt="..."/>
         <?= $news->getAuthor()->getPseudo() ?>
         <?= $news->getDateCreated() ?>
-        <a href="<?= Utils::getEnv()->getValue("PATH_SUBFOLDER") ?>news/<?= $news->getSlug() ?>"><?= $news->getTitle() ?></a>
+        <a href="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>news/<?= $news->getSlug() ?>"><?= $news->getTitle() ?></a>
 
-        <!--YOU CAN CHECK LIKE THIS FOR LIKES -->                        
+        <!--YOU CAN CHECK LIKE THIS FOR LIKES -->
         <?php if ($news->getLikes()->userCanLike()): ?>
-            <?php if(UsersController::isUserLogged()) {echo "You already love!";} else {echo "Log in to like!";} ?>
-            <?php else: ?> 
-                <a href="<?= $news->getLikes()->getSendLike() ?>">You will like</a>
-            <?php endif; ?>
+            <?php if (UsersController::isUserLogged()) {
+                echo "You already love!";
+            } else {
+                echo "Log in to like!";
+            } ?>
+        <?php else: ?>
+            <a href="<?= $news->getLikes()->getSendLike() ?>">You will like</a>
         <?php endif; ?>
 
     <?php endforeach; ?>
