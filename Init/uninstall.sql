fac19c96d4958a26ff001be91c7498db41d9b3a3
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-21 11:06:08 +0200

    [IMPROVE] Add Package uninstall

---
+++ Init/uninstall.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `cmw_news_tags_list`;
+DROP TABLE IF EXISTS `cmw_news_tags`;
+DROP TABLE IF EXISTS `cmw_news_banned_players`;
+DROP TABLE IF EXISTS `cmw_news_comments_likes`;
+DROP TABLE IF EXISTS `cmw_news_comments`;
+DROP TABLE IF EXISTS `cmw_news_likes`;
+DROP TABLE IF EXISTS `cmw_news`;
