93c3d00d228999b83460381357d908fabf48cbe4
Teyir <thomas.txr435@gmail.com>
2025-10-19 18:56:28 +0200

    [IMPROVES] uninstall.sql

--- Init/uninstall.sql
+++ Init/uninstall.sql
@@ -5,3 +5,12 @@ DROP TABLE IF EXISTS `cmw_news_comments_likes`;
 DROP TABLE IF EXISTS `cmw_news_comments`;
 DROP TABLE IF EXISTS `cmw_news_likes`;
 DROP TABLE IF EXISTS `cmw_news`;
+
+
+DELETE
+FROM cmw_core_options
+WHERE option_name IN (
+                      'news_cron_token',
+                      'news_enable_scheduled_publishing',
+                      'news_slug_prefix'
+    )
